7b33501236cd2a6d3628a963d62a22abead87e81
Robert Marczak <robert.m.1305@gmail.com>
2018-08-16 12:32:43 +0100

    Added property and the constructor.

--- src/SoundBar.php
+++ src/SoundBar.php
@@ -4,8 +4,17 @@ use App\src\interfaces\{ElectricityInterface, PlayableInterface};
 
 class Soundbar implements PlayableInterface
 {
-  public function play(ElectricityInterface $power)
+  protected $energy;
+
+  public function __construct(ElectricityInterface $energy)
   {
+    $this->energy = $energy;
+  }
+
+
+  public function play()
+  {
+    $power = $this->energy->supplyPower();
     var_dump($power .' received. Soundbar is playing.');
   }
 }
