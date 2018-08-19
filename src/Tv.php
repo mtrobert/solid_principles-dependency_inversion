9b18f4648be422b1ac68a520bc9aa95bf2a9d221
Robert Marczak <robert.m.1305@gmail.com>
2018-08-19 17:04:43 +0100

    Renamed getPower() to supplyPower() within play() method.

--- src/Tv.php
+++ src/Tv.php
@@ -4,5 +4,17 @@ use App\src\interfaces\{ElectricityInterface, PlayableInterface};
 
 class Tv implements PlayableInterface
 {
+  protected $power;
 
+  public function __construct(ElectricityInterface $power)
+  {
+    $this->power = $power;
+  }
+
+
+  public function play()
+  {
+    $power = $this->power->supplyPower();
+    var_dump($power .' received. TV is playing.');
+  }
 }
