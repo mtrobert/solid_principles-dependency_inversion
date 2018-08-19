a5d97d32f01adf2f918b57e7faa91368723a34ea
Robert Marczak <robert.m.1305@gmail.com>
2018-08-19 17:05:51 +0100

    Created diffrent objects for example purposes.

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php require 'vendor/autoload.php';
+
+use App\src\{SoundBar,Tv, Outlet,SolarEnergy, MainsPower};
+
+$energy = new SolarEnergy();
+$mainsPower = new MainsPower();
+
+$outlet = new Outlet($energy);
+
+$soundbar = new SoundBar($outlet);
+$tv = new Tv($mainsPower);
+
+
+$soundbar->play();
+echo '</br>';
+$tv->play();
